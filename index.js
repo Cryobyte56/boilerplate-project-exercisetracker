b8630c4fd8db3202e24361177af2b3298e4de985
Cryobyte56 <danielasanon54@gmail.com>
2025-04-29 08:57:22 +0800

    Completed Exercise Tracker.

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected."))
+  .catch((err) => console.error(err));
+
+// Routes
+const apiRoutes = require("./routes/api");
+app.use("/api", apiRoutes);
+
+// Index Page
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
+// Start Server
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Server listening on port " + listener.address().port);
+});
