4787f5f8c1a50da97ac854e4a3a79276356d78a9
Cryobyte56 <danielasanon54@gmail.com>
2025-04-29 09:04:47 +0800

    Mongoose Connection Changed Due to Deprecated Approach on index.js.

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected."))
-  .catch((err) => console.error(err));
+mongoose.connect(process.env.MONGO_URI);
 
 // Routes
 const apiRoutes = require("./routes/api");
